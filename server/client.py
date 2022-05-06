f543c828e223c59a1543140b431447138a616bbc
John Little <john.little@tufts.edu>
2022-05-05 23:08:46 -0400

    tried to make all of it macos and linux friendly

--- server/client.py
+++ server/client.py
@@ -1,17 +1,23 @@
 import socket
 
-host = "chat.johnsnlittle.com" #socket.gethostname()
-port = 9999                   # The same port as used by the server
+# IF YOU WANNA USE THIS SCRIPT, JUST CHANGE THE msg BELOW
+# AND RUN IT!
+
+msg = "Hello, this is a new chat. how are you?"
+
+#######################################
+
+host = "chat.johnsnlittle.com"
+port = 9999
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-msg = "Hello, this is a new chat. how are you?"
 msg_encoded = bytes(msg, 'UTF-8')
-op_code = 123
+op_code = 0
 op_code_encoded = op_code.to_bytes(1, 'big')
 length = len(msg) + 1 + 4
 length_encoded = length.to_bytes(4, 'big')
 send_data = length_encoded + op_code_encoded + msg_encoded
-print("seding len: ", len(send_data))
+print("sending ", len(send_data), " bytes to ", host, ":", port, sep="")
 s.sendall(send_data)
 
 # data = s.recv(1024)
--- server/server.cpp
+++ server/server.cpp
@@ -12,60 +12,11 @@
 
 using namespace std;
 
+int listen(const char* port);
+
 int main() {
 
-    struct addrinfo hints;
-    struct addrinfo *res;
-    int yes = 1;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    int status = getaddrinfo(NULL, PORT, &hints, &res);
-
-    if (status != 0) {
-    	fprintf(stderr, "[server] getaddrinfo error: %s\n", gai_strerror(status));
-    	exit(1);
-    }
-
-    int listener;
-    struct addrinfo *p;
-    for (p = res; p != NULL; p = p->ai_next) {
-    	listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (listener == -1) {
-        	perror("socket");
-        	continue;
-        }
-
-        if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
-        	perror("setsockopt");
-        	exit(1);
-        }
-
-        if (bind(listener, p->ai_addr, p->ai_addrlen) == -1) {
-        	close(listener);
-        	perror("bind");
-        	continue;
-        }
-
-        break;
-    }
-
-    freeaddrinfo(res);
-
-    if (p == NULL) {
-    	fprintf(stderr, "[server] failed to bind to all ip addresses.\n");
-    	exit(1);
-    }
-
-    if (listen(listener, BACKLOG_SIZE) == -1) {
-    	perror("listen");
-    	exit(1);
-    }
-
-    printf("[server] socket fd %d listening at chat.johnsnlittle.com:%s ...\n", listener, PORT);
+    int listener = listen(PORT);
 
     unordered_set<int> subscribers;
     subscribers.insert(listener);
@@ -160,5 +111,62 @@ int main() {
     }
 
     return 0;
+}
+
+int listen(const char* port) {
+    struct addrinfo hints;
+    struct addrinfo *res;
+    int yes = 1;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int status = getaddrinfo(NULL, port, &hints, &res);
+
+    if (status != 0) {
+        fprintf(stderr, "[server] getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    int listener;
+    struct addrinfo *p;
+    for (p = res; p != NULL; p = p->ai_next) {
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (listener == -1) {
+            perror("socket");
+            continue;
+        }
+
+        if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (::bind(listener, p->ai_addr, p->ai_addrlen) == -1) {
+            close(listener);
+            perror("bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(res);
+
+    if (p == NULL) {
+        fprintf(stderr, "[server] failed to bind to all ip addresses.\n");
+        exit(1);
+    }
+
+    if (listen(listener, BACKLOG_SIZE) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("[server] socket fd %d listening at localhost:%s ...\n", listener, PORT);
+
+    return listener;
 }
 
